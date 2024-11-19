f4e09bdcb0b6cbeb52862f2144c36738bcddf1d2
Ma5Codes <117619833+Ma5Codes@users.noreply.github.com>
2024-11-19 12:01:04 +0300

    Hurray!!-backend completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Login from './pages/login/Login.jsx'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="p-4 h-screen flex items-center justify-center"> 
+     <Login />
+    </div>
+  )
+}
+
+export default App;
