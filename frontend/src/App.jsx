3f29dad229eac8c8e8d3492c0a6de9e35364f307
Ma5Codes <117619833+Ma5Codes@users.noreply.github.com>
2024-11-20 11:59:09 +0300

    Hurray!!-backend completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-import Login from './pages/login/Login.jsx'
+import SignUp from './pages/signup/SignUp.jsx'
 import './App.css'
 
 function App() {
@@ -7,46 +6,9 @@ function App() {
 
   return (
     <div className="p-4 h-screen flex items-center justify-center"> 
-     <Login />
+     <SignUp />
     </div>
   )
 }
 
 export default App;
-=======
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
->>>>>>> e2ac6dc (Add project code)
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0">
+      <Sidebar />
+      {/*MeessageContainer*/}
+      
     </div>
   )
 }
